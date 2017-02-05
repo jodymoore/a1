9cb64996fac96f48c2e5fb890caea5dd03c8c61b
Jody Moore <jodymoore@g.harvard.edu>
2017-02-05 13:58:15 -0600

    changes to index removed inline styling

--- index.php
+++ index.php
@@ -2,50 +2,29 @@
   <head>
 	  <title>Jody Moore</title>
 
-	  <link rel="stylesheet" href="" type="text/css">
-	  <link href="" rel="stylesheet">
-       <style type="text/css">
-  
-		#div1 img {
-			display: block;
-		    margin-left: auto;
-		    margin-right: auto;
-		}
-		#div1 h2{
-		  text-align: center;
-		}
-		#div1 h1{
-		  text-align: center;
-		}
-		#div1 blockquote{
-		  text-align: center;
-		}
-		#div1 p{
-		  display: inline-block;
-          margin: 200px 0 0 73px;
-		}
+	  <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+      <link href="/css/bootstrap-theme.min.css" rel="stylesheet"/>
+      <link href="/css/styles.css" rel="stylesheet"/>
 
-	   </style>
-
-	  <link href="/css/styles.css" rel="stylesheet">
+	  <script type="text/javascript" src="js/jquery-1.8.0.min.js"></script> 
+      <script src="/js/jquery-1.10.2.min.js"></script>
+      <script src="/js/bootstrap.min.js"></script>
+      <script src="/js/scripts.js"></script>
 
   </head>
   <body>
     <div id="div1"> 
 		  <h1>Jody Moore</h1>
 
-		  <img id="pic" src="img/myPic2.jpg" alt="Jody Moore">
+		  <img id="pic"src="img/myPic2.jpg" alt="Jody Moore">
 
 		  <h2>About Me</h2>
 		  <p>			
-          My name is Jody Moore I currently live in Ft Smith Ar. In the past Three years i discovered my interest in programming. I have just completed a programming certificate from Harvard Extension School. I have recently received my Amazon Web Services Solutions Architect-Associate Certification. This course will help improve my PHP, Laravel, and CSS skills. I look Forward to working on an E-commerce type site.
+          My name is Jody Moore.  I currently live in Ft. Smith Arkansas.  In the past three years I've discovered my interest in programming. <br>I have just completed a programming certificate from Harvard Extension School and have recently received my Amazon<br> Web Services Solutions Architect-Associate Certification.  This course will help improve my PHP, Laravel, and CSS skills.<br>I look forward to working on an E-commerce style site.
 		  </p>
 		  <h2>Random Quote</h2>
 		  <blockquote>
 		  	Anyone who has never made a mistake has never tried anything new.-Albert Einstein</blockquote>
 	</div>
-
-	  <script src=""></script>
-
   </body>
 </html>
