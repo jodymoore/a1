2e7aa6d02748f1f14f1fcd845a3f07777b07f6bb
Jody Moore <jodymoore@g.harvard.edu>
2017-02-05 18:50:27 -0600

    change to index

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+
+<?php require('logic.php'); ?>
+
 <html>
   <head>
       <title>Jody Moore</title>
@@ -9,21 +12,20 @@
       <script src="/js/jquery-1.10.2.min.js"></script>
       <script src="/js/bootstrap.min.js"></script>
       <script src="/js/scripts.js"></script>
-
   </head>
   <body>
     <div id="div1"> 
 		  <h1>Jody Moore</h1>
 
-		  <img id="pic"src="img/myPic2.jpg" alt="Jody Moore">
+		  <img src="img/myPic3.jpg" alt="Jody Moore">
 
 		  <h2>About Me</h2>
-		  <p>			
-          My name is Jody Moore.  I currently live in Ft. Smith Arkansas.  In the past three years I've discovered my interest in programming. <br>I have just completed a programming certificate from Harvard Extension School and have recently received my Amazon<br> Web Services Solutions Architect-Associate Certification.  This course will help improve my PHP, Laravel, and CSS skills.<br>I look forward to working on an E-commerce style site.
-		  </p>
+		  <h5><p>			
+          My name is Jody Moore.  In the past three years I've discovered my interest in programming. I have just completed a <br>programming certificate from Harvard Extension School and have recently received my Amazon Web Services Solutions <br>Architect-Associate Certification.  This course will help improve my PHP, Laravel, and CSS skills. I look forward to working <br>on an E-commerce style site.
+		  </p></h5>
 		  <h2>Random Quote</h2>
 		  <blockquote>
-		  	Anyone who has never made a mistake has never tried anything new.-Albert Einstein</blockquote>
+		  	<h4><?php echo $RandomQoute; ?></h4></blockquote>
 	</div>
   </body>
 </html>
