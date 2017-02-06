483e7a213b099e535154736e8a6c2667def83d17
Jody Moore <jodymoore@g.harvard.edu>
2017-02-06 09:19:44 -0600

    added qoutes

--- logic.php
+++ logic.php
@@ -2,7 +2,7 @@
 
 	$RandomQoute;
 
-	$qoutes[] = array("Anyone who has never made a mistake has never tried anything new. - Albert Einstein", "Imagination is more important than knowledge. - Albert Einstein", "Everything should be made as simple as possible, but not simpler. - Albert Einstein");
+	$qoutes[] = array('"Anyone who has never made a mistake has never tried anything new." - Albert Einstein', '"Imagination is more important than knowledge." - Albert Einstein', '"Everything should be made as simple as possible, but not simpler." - Albert Einstein');
 	
 	$rand = rand(0,2);
 
