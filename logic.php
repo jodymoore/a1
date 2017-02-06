683ef54d690cf062a11e0ce19a82dd935a7d0e39
Jody Moore <jodymoore@g.harvard.edu>
2017-02-05 21:28:25 -0600

    initial commit

---
+++ logic.php
@@ -0,0 +1,11 @@
+<?php
+
+$RandomQoute;
+
+$qoutes[] = array("Anyone who has never made a mistake has never tried anything new. - Albert Einstein", "Imagination is more important than knowledge. - Albert Einstein", "Everything should be made as simple as possible, but not simpler. - Albert Einstein");
+$rand = rand(0,2);
+
+$RandomQoute = $qoutes[0][$rand];
+
+
+?>
